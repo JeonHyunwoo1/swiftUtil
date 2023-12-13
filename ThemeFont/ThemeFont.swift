b5ca7ec5b68ba876aabd49c4d1a7efbb7b478ad5
jhw <unity3013@dkitec.com>
2023-12-13 16:00:12 +0900

    FontUtil

---
+++ ThemeFont/ThemeFont.swift
@@ -0,0 +1,23 @@
+//
+//  ThemeFont.swift
+//  tip-calculator
+//
+//  Created by Hyunwoo Jeon on 11/21/23.
+//
+
+import UIKit
+
+struct ThemeFont {
+    // AvenirNext
+    static func regular(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-Regular", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+    static func bold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-Bold", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+    static func demibold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-DemiBold", size: size) ?? .systemFont(ofSize: size)
+    }
+}
